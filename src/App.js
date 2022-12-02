1230ce2f1ec0963222aa1efb72536187bcf35966
shaikat17 <shaikatpal56@gmail.com>
2022-12-02 11:54:18 +0600

    Added Link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Products from "./Components/Products";
+import Error from "./Components/Error";
+
+
 
 function App() {
   return (
     <BrowserRouter>
+    <nav>Our Navbar</nav>
       <Routes>
-      <Route path="/" element={<div>Home page</div>} />
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="products" element={<Products />} />
+      <Route path="*" element={<Error />} />
       </Routes>
+      <footer>Our Footer</footer>
     </BrowserRouter>
   );
 }
